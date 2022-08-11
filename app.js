525887f33d9ea8b160fbf448512bc7a5d76904fa
Kyle McCall <kylem147@gmail.com>
2022-08-11 11:10:04 -0700

    sets up html and css layout

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// import functions and grab DOM elements
+const pollForm = document.getElementById('poll-form');
+const optionAButon = document.getElementById('option-a-button');
+const optionBButon = document.getElementById('option-b-button');
+const subtractA = document.getElementById('subtract-a-button');
+const subtractB = document.getElementById('subtract-b-button');
+const submitPollButton = document.getElementById('submit-poll-button');
 
-// let state
+let question = '';
+let optionA = '';
+let optionB = '';
+let votesA = 0;
+let votesB = 0;
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+pollForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(pollForm);
+
+    question = data.get('question');
+    optionA = data.get('option-a');
+    optionB = data.get('option-b');
+
+    console.log(question, optionA, optionB);
+    
+});
